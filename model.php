c4982babd141a69fc44f3d39c7ff4e0f70b23364
Anon Ymous <Anon Ymous>
2017-01-09 23:36:41 -0500

    Model basically done

--- model.php
+++ model.php
@@ -134,34 +134,4 @@ class Model {
 	public static $table;
 }
 
-class User extends Model {
-	public static $data=[
-		'username'=>[
-			'datatype'=>'varchar(64)',
-			'maxlength'=>64,
-			'minlength'=>1,
-			'required'=>true,
-			'unique'=>true
-		],
-		
-		'password'=>[
-			'datatype'=>'varchar(255)',
-			'minlength'=>5,
-			'prepare'=>'CMS::passwordHash',
-			'prepare_after'=>true,
-			'required'=>true
-		]
-	];
-	
-	public static $table='users';
-}
-
-$user=new User();
-$user->username=base64_encode(rand());
-$user->password='boobs';
-
-if(is_array($errors=$user->save())){
-	die(CMS::debug($errors));
-}
-
 ?>
